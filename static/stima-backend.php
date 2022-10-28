3e4e384c1bbec778db19ea38b81a7503a3893b3d
PasqualePaliotta <114742509+PasqualePaliotta@users.noreply.github.com>
2022-10-28 16:16:05 +0200

    css finito paliotta

--- static/stima-backend.php
+++ static/stima-backend.php
@@ -2,6 +2,42 @@
 
 stima dei tempi per il backend : 4 ore (Armenise Gabriele)
 
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="CSS/frontheadPALIOTTA">
+    <link href="PHP/index.php">
+</head>
+<body>
+
+	<h1>Quanto sarai povero?</h1>
+	<h2>Giorni</h2>
+	<input type="number" class="t1" name="giorni" placeholder="Quanti giorni durera' il lavoro?">
+
+	<hr class="linea1">
+
+	<h3>Campo obbligatorio</h3>
+
+	<h4>Ore giornaliere di lavoro</h4>
+
+	<input type="number" class="t2" name="ore" placeholder="Quanti lavori al giorno?">
+
+	<hr class="linea2"> 
+
+	<h5>Campo obbligatorio</h5>
+
+	<h6>Compenso orario</h6>
+
+	<input type="number" class="t3" name="pagamentoh" placeholder="Quanti vieni pagato all'ora?">
+
+	<hr class="linea3">
+
+	<h7>Campo obbligatorio</h7>
+
+	<input type="submit" class="bottone" name="bottone" value="CALCOLA">
+</body>
+</body>
+
+
 
 
 
